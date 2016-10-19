da085d1f21b80be34011f69416fb944cdc28e047
marcelo2012 <mscaldas@gmail.com>
2016-10-19 17:11:12 -0400

    First commit

---
+++ src/main/java/edu/msc/spring/bootstrap/SpringRESTBootstrap.java
@@ -0,0 +1,13 @@
+package edu.msc.spring.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRESTBootstrap {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRESTBootstrap.class, args);
+	}
+
+}
