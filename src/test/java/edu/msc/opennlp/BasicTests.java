09e8436592832b34295770f95a5a9454494dbfa9
marcelo2012 <mscaldas@gmail.com>
2017-01-06 11:51:23 -0500

    adding sentence detection and Name recognition tests

--- src/test/java/edu/msc/opennlp/BasicTests.java
+++ src/test/java/edu/msc/opennlp/BasicTests.java
@@ -1,12 +1,15 @@
 package edu.msc.opennlp;
 
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.Span;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.FileInputStream;
@@ -17,27 +20,108 @@ import java.io.InputStream;
 /**
  * Created by caldama on 11/29/2016.
  */
+//Testing from https://opennlp.apache.org/documentation/1.6.0/manual/opennlp.html
+//Models to download: http://opennlp.sourceforge.net/models-1.5/
+
 @RunWith(SpringJUnit4ClassRunner.class)
-//@SpringBootTest
 public class BasicTests {
+
+    public static final String PHRASE_TEST = "Pierre Vinken, 61 years old, and Marcelo Caldas will join the board as a nonexecutive director Nov. 29. Mr. Vinken is " +
+            "chairman of Elsevier N.V., the Dutch publishing group. Rudolph Agnew, 55 years " +
+            "old and former chairman of Consolidated Gold Fields P.L.C., was named a director of this " +
+            "British industrial conglomerate.";
 
     @Test
     public void testTokenization() {
         InputStream modelIn = null;
         try {
-            modelIn = new FileInputStream("C:/_projects/springRestBootstrap/src/test/resources/en-token.bin");
+            modelIn = new FileInputStream("src/test/resources/en-token.bin");
             TokenizerModel model = new TokenizerModel(modelIn);
             Tokenizer tokenizer = new TokenizerME(model);
-            String tokens[] = tokenizer.tokenize("An input sample sentence.");
+            String tokens[] = tokenizer.tokenize(PHRASE_TEST);
             for( String t: tokens) {
                 System.out.println("t = " + t);
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }catch (IOException e) {
             e.printStackTrace();
         }
 
+        finally {
+            if (modelIn != null) {
+                try {
+                    modelIn.close();
+                }
+                catch (IOException e) {
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testSentenceDetection() throws FileNotFoundException {
+        InputStream modelIn = new FileInputStream("src/test/resources/en-sent.bin");
+
+
+        try {
+            SentenceModel model = new SentenceModel(modelIn);
+            SentenceDetectorME sentenceDetector = new SentenceDetectorME(model);
+            String sentences[] = sentenceDetector.sentDetect(PHRASE_TEST);
+            for (String s: sentences) {
+                System.out.println("s:" + s);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (modelIn != null) {
+                try {
+                    modelIn.close();
+                }
+                catch (IOException e) {
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testNameEntityRecognizer() throws FileNotFoundException {
+        InputStream modelIn = new FileInputStream("src/test/resources/en-ner-person.bin");
+        InputStream sentencemodelIn = new FileInputStream("src/test/resources/en-sent.bin");
+        InputStream tokenmodelIn = new FileInputStream("src/test/resources/en-token.bin");
+
+        try {
+            TokenNameFinderModel model = new TokenNameFinderModel(modelIn);
+            NameFinderME nameFinder = new NameFinderME(model);
+
+            SentenceModel sentencemodel = new SentenceModel(sentencemodelIn);
+            SentenceDetectorME sentenceDetector = new SentenceDetectorME(sentencemodel);
+            String sentences[] = sentenceDetector.sentDetect(PHRASE_TEST);
+
+            TokenizerModel tokenmodel = new TokenizerModel(tokenmodelIn);
+            Tokenizer tokenizer = new TokenizerME(tokenmodel);
+
+            for (String s: sentences) {
+                String tokens[] = tokenizer.tokenize(s);
+                Span nameSpans[] = nameFinder.find(tokens);
+                if (nameSpans != null && nameSpans.length > 0) {
+                    for (Span sp : nameSpans) {
+                        int idx = sp.getStart();
+                        System.out.print("name: ");
+                        for (idx = sp.getStart(); idx < sp.getEnd(); idx++) {
+                            System.out.print(tokens[idx] + " ");
+                        }
+                        System.out.println();
+                    }
+                } else {
+                    System.out.println("No names found in sentence -> " + s);
+                }
+                // do something with the names
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
         finally {
             if (modelIn != null) {
                 try {
